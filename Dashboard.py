098ceb8af1a4e4144a2aa2750fe9c5f4dab42935
Lailaa06 <81522385+Lailaa06@users.noreply.github.com>
2025-03-13 19:39:30 +0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -118,4 +118,4 @@ with tab2:
     # Rename column untuk tampilan yang lebih jelas
     top_products.rename(columns={'product_category_name': 'Product Category', 'product_id': 'Product ID'}, inplace=True)
     
-    st.write(top_products[['Product ID', 'Product Category', 'price']])
+    st.write(top_products[['Product ID', 'Product Category','price']])
